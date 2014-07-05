086b117e9452aaacdc876ee96204d1575a727703
Gilbert Graves <egg@metrobg.com>
2014-07-05 12:15:42 -0400

    Initial version of this program.

---
+++ AndroidSetup.sh
@@ -0,0 +1,13 @@
+
+mv /tmp/AR/AR-BUYPROF.1 /tmp/AR-BUYPROF
+mv /tmp/AR/AR-CCLASS.1 /tmp/AR-CCLASS
+mv /tmp/AR/AR-CTYPE.1 /tmp/AR-CTYPE
+mv /tmp/AR/AR-CUSMAS.1 /tmp/AR-CUSMAS
+mv /tmp/AR/AR-SLSMAN.1 /tmp/AR-SLSMAN
+mv /tmp/OE/OE-PROMO.1 /tmp/OE-PROMO
+mv /tmp/IV/IV-PCLASS.1 /tmp/IV-PCLASS
+mv /tmp/IV/IV-ITMFIL.1 /tmp/IV-ITMFIL
+mv /tmp/IV/IV-MCODE.1  /tmp/IV-MCODE
+mv /tmp/IV/IV-STATUS.1  /tmp/IV-STATUS
+
+rm -rf /tmp/AR /tmp/IV /tmp/OE
