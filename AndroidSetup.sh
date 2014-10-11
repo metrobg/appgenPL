98a3b92e0d1abdb56cc1c255aabcc10aa4169b43
Gilbert Graves <egg@metrobg.com>
2014-10-11 11:33:51 -0400

    Changed order of processing to match table order in the database, added a processing rule to move the OE-ITMHIST file.

--- AndroidSetup.sh
+++ AndroidSetup.sh
@@ -1,13 +1,14 @@
-
+mv /tmp/AP/AP-VENFIL.1 /tmp/AP-VENFIL
 mv /tmp/AR/AR-BUYPROF.1 /tmp/AR-BUYPROF
 mv /tmp/AR/AR-CCLASS.1 /tmp/AR-CCLASS
 mv /tmp/AR/AR-CTYPE.1 /tmp/AR-CTYPE
 mv /tmp/AR/AR-CUSMAS.1 /tmp/AR-CUSMAS
 mv /tmp/AR/AR-SLSMAN.1 /tmp/AR-SLSMAN
-mv /tmp/OE/OE-PROMO.1 /tmp/OE-PROMO
-mv /tmp/IV/IV-PCLASS.1 /tmp/IV-PCLASS
 mv /tmp/IV/IV-ITMFIL.1 /tmp/IV-ITMFIL
 mv /tmp/IV/IV-MCODE.1  /tmp/IV-MCODE
+mv /tmp/IV/IV-PCLASS.1 /tmp/IV-PCLASS
 mv /tmp/IV/IV-STATUS.1  /tmp/IV-STATUS
-mv /tmp/AP/AP-VENFIL.1 /tmp/AP-VENFIL
-rm -rf /tmp/AR /tmp/IV /tmp/OE /tmp/AP
+mv /tmp/IV/IV-UOFM.1 /tmp/IV-UOFM
+mv /tmp/OE/OE-ITMHIST.1 /tmp/OE-ITMHIST
+mv /tmp/OE/OE-PROMO.1 /tmp/OE-PROMO
+rm -rf /tmp/AP /tmp/AR /tmp/IV /tmp/OE 
