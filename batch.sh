c6dcea9d172e6fa2e87d42c5e757e8e67b1eb2d5
Gilbert Graves <egg@metrobg.com>
2014-10-11 13:18:13 -0400

    Update batch.sh
    does not contain DB host parameter or DB password

--- batch.sh
+++ batch.sh
@@ -1,16 +1,16 @@
 /home/ggraves/scripts/AndroidSetup.sh
 sftp -b /home/ggraves/scripts/cmds ggraves@pico.metrobg.com
 
-/home/ggraves/scripts/loadIV-ADDL.pl   64.31.183.210 wigrodma 
-/home/ggraves/scripts/loadAP-VENFIL.pl 64.31.183.210 wigrodma 
-/home/ggraves/scripts/loadAR-CCLASS.pl 64.31.183.210 wigrodma
-/home/ggraves/scripts/loadAR-CTYPE.pl  64.31.183.210 wigrodma
-/home/ggraves/scripts/updAR-CUSMAS.pl  64.31.183.210 wigrodma
-/home/ggraves/scripts/loadAR-CUSMAS.pl 64.31.183.210 wigrodma
-/home/ggraves/scripts/loadAR-SLSMAN.pl 64.31.183.210 wigrodma
-/home/ggraves/scripts/loadIV-ITMFIL.pl 64.31.183.210 wigrodma 
-/home/ggraves/scripts/loadIV-MCODE.pl  64.31.183.210 wigrodma 
-/home/ggraves/scripts/loadIV-PCLASS.pl 64.31.183.210 wigrodma
+/home/ggraves/scripts/loadIV-ADDL.pl    
+/home/ggraves/scripts/loadAP-VENFIL.pl  
+/home/ggraves/scripts/loadAR-CCLASS.pl 
+/home/ggraves/scripts/loadAR-CTYPE.pl  
+/home/ggraves/scripts/updAR-CUSMAS.pl  
+/home/ggraves/scripts/loadAR-CUSMAS.pl 
+/home/ggraves/scripts/loadAR-SLSMAN.pl 
+/home/ggraves/scripts/loadIV-ITMFIL.pl  
+/home/ggraves/scripts/loadIV-MCODE.pl   
+/home/ggraves/scripts/loadIV-PCLASS.pl 
 AGHOME=/home/ag6;TERM=xterm;export AGHOME TERM;/home/ag6/bin/show /tmp/IV-STATUS > /tmp/iv-keys
-/home/ggraves/scripts/loadIV-STATUS.pl 64.31.183.210 wigrodma
-/home/ggraves/scripts/loadIV-UOFM.pl   64.31.183.210 wigrodma
+/home/ggraves/scripts/loadIV-STATUS.pl 
+/home/ggraves/scripts/loadIV-UOFM.pl   
